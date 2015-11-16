5b712dc98be72f749bc1d3cc9f8fbf346650c7f5
Victor Pudeyev <piousbox@Victors-MacBook-Pro.local>
2015-11-16 13:46:40 -0800

    better install script for nexus

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,19 +1,31 @@
 
-execute "download install Nexus OSS" do
+execute "download Nexus OSS" do
   command <<-EOL
-    wget #{node[:nexus][:url]}
-    tar -xvf nexus-#{node[:nexus][:external_version]}-bundle.tar.gz
-    mv nexus-#{node[:nexus][:external_version]}-bundle.tar.gz /opt
-    ln -s nexus-#{node[:nexus][:external_version]} nexus
-EOL
+    wget http://www.sonatype.org/downloads/nexus-latest-bundle.tar.gz;
+  EOL
+  not_if { ::File.exists?('/opt/nexus-latest-bundle.tar.gz') }
+  cwd "/opt"
+end
+
+execute "unpack Nexus OSS" do
+  command <<-EOL
+    rm -frv trash; mkdir -p trash; 
+    tar --directory trash -xvf nexus-latest-bundle.tar.gz;
+  EOL
+  cwd "/opt"
+end
+
+execute "move Nexus OSS" do
+  command "mv /opt/trash/nexus-* .; ln -s nexus-* nexus"
   cwd "/usr/local"
 end
 
 execute "run Nexus OSS" do
   command <<-EOL
     ./nexus start
-EOL
+  EOL
   cwd "/usr/local/nexus/bin"
+  user node[:nexus][:user]
 end
 
 # set NEXUS_HOME ?
